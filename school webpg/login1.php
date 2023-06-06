8075841106792c2fe00cf4795582f3c9a4453d3d
shruuti321 <114770273+shruuti321@users.noreply.github.com>
2023-06-06 18:55:29 +0530

    Add files via upload

---
+++ school webpg/login1.php
@@ -0,0 +1,34 @@
+<?php      
+    /*include('connection.php');  
+    $fid = $_POST['fid'];  
+    $pswd = $_POST['pswd'];  
+      
+        //to prevent from mysqli injection  
+       /* $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  */
+      
+       /* $sql = "select *from details where formid = '$fid' and pwd = '$pswd'";  
+        $result = mysqli_query($con, $sql);  
+        //$row = mysqli_fetch_array($result);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> Login successful </center></h1>";  
+        }  
+        else{  
+            echo "<h1> Login failed. Invalid username or password.</h1>";  
+        }  */   
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Title of the document</title>
+  </head>
+  <body>
+    <button onclick="window.location.href='https://w3docs.com';">
+      Click Here
+    </button>
+  </body>
+</html>
---
+++ school webpg/registration.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect("localhost","root","","school");
+if(!$con)
+{
+        echo "error";
+}
+?>
